635e4ef566eb3b9c2b4874b77fa57d9bd3b1f359
Vitor Hugo <vto.hugo67@gmail.com>
2023-08-10 19:07:25 -0300

    DPT-015<feat> Banking Page Updates

---
+++ src/components/cards/common/TransferItem.vue
@@ -0,0 +1,61 @@
+
+<template>
+    <!-- ITEM -->
+    <div class="flex items-start justify-between text-center md:text-start px-6 md:px-3 mb-5">
+          <div class="relative">
+              <font-awesome-icon :icon="icon" :class="typeClass" />
+              <div class="flex-start md:pl-10">
+                  <p class="text-md text-BBDark">{{ name }}</p>
+                  <p class="text-xs text-BBDark"><b>{{ method }}</b> {{ type }}</p>
+              </div>
+          </div>
+          <div class="flex-end">
+              <p class="text-xs md:text-end text-BBDark">  <font-awesome-icon icon="calendar-days" class="text-gray-500 text-md cursor-pointer mr-1" />{{ date }}</p>
+              <p :class="moneyClass"><b><font-awesome-icon icon="money-bill-transfer" class="text-xs cursor-pointer mr-1 pt-2" /></b>{{ value }}</p>
+          </div>
+      </div>
+      <!-- ITEM -->
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+export default {
+  name: 'TransferItem',
+  props: {
+      name: {
+          type: String,
+          default: 'N/A'
+      }, 
+      method: {
+          type: String,
+          default: 'N/A'
+      },
+      type: {
+          type: String,
+          default: 'N/A'
+      },
+      date: {
+          type: String,
+          default: '00/00/1999'
+      },
+      value: {
+          type: String,
+          default: 'R$0.000,00'
+      }
+  },
+  computed: {
+      icon() {
+          return this.type.toLowerCase() == 'received' ? 'circle-arrow-up' : 'circle-arrow-down';
+      },
+      moneyClass(){
+          return this.type.toLowerCase() == 'received' ? "text-xs text-green-700 text-center md:text-end" : "text-xs text-danger text-center md:text-end"
+      },
+      typeClass(){
+        return this.type.toLowerCase() == 'received' ? "md:absolute hidden md:block text-green-700 text-3xl cursor-pointer pt-2" : "md:absolute hidden md:block text-danger text-3xl cursor-pointer pt-2"
+      }
+  },
+  components: {
+      FontAwesomeIcon
+  }
+}
+</script>
--- src/views/Banking.vue
+++ src/views/Banking.vue
@@ -1,17 +1,17 @@
 <template >
     <!-- ADD NEW INVESTMENT AREA -->
     <div class="mt-2">
-            <div class="flex pl-5 flex-wrap md:-mx-4">
+            <div class="flex p-2 md:pl-5 flex-wrap md:-mx-4">
                 <!-- START OVERVIEW AREA -->
-                <div class="w-full md:w-2/2 lg:w-1/3 mb-5 bg-purple-100">
+                <div class="w-full md:w-2/2 lg:w-1/3 mb-5 md:bg-purple-100">
                     <img src="@/assets/bgvector.png" class="rounded- w-screen h-64 hidden md:block "> 
-                    <div class="hidden md:relative md:flex flex-col flex-auto min-w-0 md:p-4 md:mx-6 md:-mt-24 overflow-hidden break-words border-0 shadow-blur rounded-2xl bg-white bg-clip-border backdrop-blur-2xl backdrop-saturate-200">
+                    <div class=" md:relative md:flex flex-col shadow md:shadow-transparent flex-auto min-w-0 p-5 md:p-4 md:mx-6 md:-mt-24 overflow-hidden break-words border-0 shadow-blur rounded-2xl bg-white bg-clip-border backdrop-blur-2xl backdrop-saturate-200">
                         <div class="md:flex md:flex-col">
                             <div class="md:flex md:items-center text-center md:text-start">
-                                <div class="flex-shrink-0 rounded-3xl shadow p-1 bg-white mr-2 w-32 items-center text-center hidden md:block">
+                                <div class="flex-shrink-0 rounded-3xl shadow p-1  bg-white mr-2 w-32 items-center text-center hidden md:block">
                                     <font-awesome-icon icon="bank" class="text-5xl text-purple-700 m-5" />
                                 </div> 
-                                <div class="flex-grow ml-5 mt-10 md:mt-0">
+                                <div class="flex-grow ml-5 my-2 md:mt-0">
                                 <p class="text-gray-600 text-xs">Account Balance</p>
                                 <h2 class="text-xl font-semibold mb-2">R$00.000,00</h2>
                                 <p class="text-gray-600 text-xs">This is your actual account ballance.</p>
@@ -19,38 +19,60 @@
                         </div>
                     </div>
                 </div>
-                <div class="hidden md:relative md:flex flex-col flex-auto min-w-0  md:p-4 md:mx-6 md:my-5 overflow-hidden break-words border-0 shadow-blur rounded-2xl bg-white bg-clip-border backdrop-blur-2xl backdrop-saturate-200">
+                <div class=" md:relative md:flex flex-col shadow md:shadow-transparent flex-auto min-w-0 mt-2  md:p-4 md:mx-6 md:my-5 overflow-hidden break-words border-0 shadow-blur rounded-2xl bg-white bg-clip-border backdrop-blur-2xl backdrop-saturate-200">
                         <div class="md:flex md:flex-col">
                             <div class="md:flex md:items-center text-center md:text-start">
                                 <div class="flex-shrink-0 rounded-3xl shadow p-1 bg-white mr-2 w-32 items-center text-center hidden md:block">
                                     <font-awesome-icon icon="clock" class="text-5xl text-purple-700 m-5" />
                                 </div> 
-                                <div class="flex-grow ml-5 mt-10 md:mt-0">
-                                <p class="text-gray-600 text-xs">Account Loans</p>
-                                <h2 class="text-xl font-semibold mb-2">R$00.000,00</h2>
-                                <p class="text-gray-600 text-xs">This is related to your loan debits.</p>
-                            </div>
+                                <div class="flex-grow ml-5 my-2 md:mt-0">
+                                    <p class="text-gray-600 text-xs">Account Loans</p>
+                                    <h2 class="text-xl font-semibold mb-2">R$00.000,00</h2>
+                                    <p class="text-gray-600 text-xs">This is related to your loan debits.</p>
+                                </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- END OVERVIEW AREA -->
-            <div class="w-full md:w-1/2 lg:w-2/3 px-4 mb-5">
+            <!-- RECENT TRANSACTIONS -->
+            <div class="w-full md:w-1/2 lg:w-1/3 md:px-4 mb-5">
+             <div class="bg-white rounded-lg shadow py-4 h-full">
+                <div class="flex items-start justify-between text-center px-4 pb-4 md:p-3">
+                    <div class="flex-start">
+                       <p class="text-xl text-BBDark">Recent Transations</p>
+                    </div> 
+                    <div class="flex-end">
+                        <font-awesome-icon icon="file-circle-plus" class="text-BBPurple text-2xl cursor-pointer" />
+                    </div>  
+                </div>
+                <hr class="h-px mx-3 bg-purple-200 border-0 mb-5" />
+                <!-- ITEM -->
+                <TransferItem
+                    v-for="(item, index) in transferItems"
+                    :key="index"
+                    :name="item.name"
+                    :method="item.method"
+                    :type="item.type"
+                    :date="item.date"
+                    :value="item.value"
+                />
+              </div>
+            </div>
+            <!-- ACCOUNT BALANCE -->
+            <div class="w-full md:w-1/2 lg:w-1/3 mb-5">
              <div class="bg-white rounded-lg shadow p-4 h-full">
                 <div class="flex flex-col items-center  text-center  m-12">
-                    <div
-                    class="rounded-md bg-gradient-to-tr from-purple-900 to-purple-500 hover:from-purple-800 hover:to-purple-500 transition duration-500 ease-in-out shadow"
-                    >
-                    <font-awesome-icon icon="money-bill-trend-up" class="text-white text-5xl p-6" />
+                    <div class="rounded-md bg-gradient-to-tr from-purple-900 to-purple-500 hover:from-purple-800 hover:to-purple-500 transition duration-500 ease-in-out shadow">
+                      <font-awesome-icon icon="money-check" class="text-white text-5xl p-6" />
                     </div>
                     <div class="text-md text-gray-700 font-medium mt-2">
-                    Add New Investment
+                      Edit Account Balance
                     </div>
                     <div class="text-xs font-light text-gray-500 mb-5 mt-1">
-                    Want to add a new investment to track? Click the button below.
+                      Want to add a new investment to track? Click the button below.
                     </div>
                     <div class="w-64">
-                        <BasicButton  active="true" text="Add New Investment" icon="plus" />
+                        <BasicButton active="true" text="Add New Investment" icon="plus" />
                     </div>  
                 </div>
               </div>
@@ -62,22 +84,48 @@
    
    <script>
    import BasicButton from '@/components/button/BasicButton.vue';
+   import TransferItem from '@/components/cards/common/TransferItem.vue';
    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
    export default {
      name: 'DashboardInvestments',
      components: {
        BasicButton,
+       TransferItem,
        FontAwesomeIcon,
      },
      data() {
        return {
          progress: 95,
-         selectOptions: [
-             { label: 'CDB/RDB Application', value: 'option1' },
-             { label: 'Stock Market', value: 'option2' },
-             { label: 'FIIS', value: 'option3' },
-         ],
-         selectedValue: '',
+         transferItems: [
+            {
+                name: 'Vitor Hugo',
+                method: 'Pix',
+                type: 'Outgoing',
+                date: '12/03/2023',
+                value: 'R$1000,00'
+            },
+            {
+                name: 'Elisyum LTDA',
+                method: 'Pix',
+                type: 'Received',
+                date: '11/03/2023',
+                value: 'R$1000,00'
+            },
+            {
+                name: 'Robert Jr',
+                method: 'Pix',
+                type: 'Outgoing',
+                date: '12/03/2023',
+                value: 'R$1000,00'
+            },
+            {
+                name: "Urubu's Pix Bank",
+                method: 'Pix',
+                type: 'Received',
+                date: '12/03/2023',
+                value: 'R$2000,69'
+            },
+        ]
        };
      },
    }
