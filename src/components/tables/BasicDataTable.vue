5d9e79af97e96c66eb7424610082396da3b70646
Vitor Hugo <vto.hugo67@gmail.com>
2023-08-10 20:43:33 -0300

    DPT-017 <feat> Banking Adjustments

--- src/components/tables/BasicDataTable.vue
+++ src/components/tables/BasicDataTable.vue
@@ -1,7 +1,30 @@
 <template>
-    <div class="w-auto bg-white">
+    <div class="w-full">
+        <div class="bg-white shadow-md rounded my-6">
+           <table class="w-full table-auto">
+                <thead>
+                    <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                    <th class="py-3 px-6 text-left">Name</th>
+                    <th class="py-3 px-6 text-left">Email</th>
+                    <th class="py-3 px-6 text-center">Status</th>
+                    <th class="py-3 px-6 text-center">Role</th>
+                    <th class="py-3 px-6 text-center">Actions</th>
+                    </tr>
+                </thead>
+                <tbody class="text-gray-600 text-sm font-light">
+                    <tr class="border-b border-gray-200 hover:bg-gray-100">
+                        <td class="py-3 px-6 text-left whitespace-nowrap">John Doe</td>
+                        <td class="py-3 px-6 text-left">johndoe@example.com</td>
+                        <td class="py-3 px-6 text-center">
+                            <span class = "bg-green-200 text-green-600 py-1 px-3 rounded-full text-xs">Active</span>
+                            </td>
+                            <td class="py-3 px-6 text-center">
 
-
+                            </td>
+                    </tr>
+                </tbody>
+           </table>
+        </div>
     </div>
 </template>
 
